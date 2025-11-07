151792da4aa26d0c5c3bb14cd93855d1c72007fb
Jhon C <alejandrotareas029@gmail.com>
2025-11-07 13:55:38 -0500

    v0.3_creacionInterfacesPaqueteRepositorio_Sauloconz21_07112025

---
+++ src/main/java/com/sena/springecommerce/repositorio/IUsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.sena.springecommerce.repositorio;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sena.springecommerce.model.Usuario;
+
+@Repository
+public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+	Optional<Usuario> findByEmail(String email);
+
+}
